551941e13a910c51261c17889f987ccd1337e1c3
MuhammadAtif66 <muhammadatifrizwan66gmail.com>
2023-09-25 23:03:56 -0700

    assignment-first

---
+++ index.js
@@ -0,0 +1,132 @@
+
+
+
+// CHAPTER-1
+
+
+// alert("Welcome")
+// alert("Error! Please entre a valid password")
+// alert("Welcome to JS Land...\n Happy Coding! ")
+// alert("Welcome to JS land...")
+// alert("Happy Coding!")
+// alert("Hello...I can run JS through my web browser's console")
+
+
+// CHAPTER-2
+
+// var username
+// var myname="M.Atif"
+// var message="Hello World"
+// alert(message)
+// var name="Jhone Doe"
+// alert(name)
+// var age="15 years old"
+// alert(age)
+// var application="Certified Mobile Application Development"
+// alert(application)
+// var pizza="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+// var email= "muhammadatifrizwan66@gmail.com"
+// var b="My email  adderss is "
+// alert(b+email)
+
+// var book="A smarter way to learn JavaScript"
+// var c="I am trying to learn from the Book "
+// alert(c+book)
+// document.write("<h2> Yah! I can write HTML content through JavaScript </h2>")
+
+// CHAPTER-3
+
+
+// var age="I am 18 years old"
+// alert(age)
+
+// if(localStorage.getItem("visitCount")=== null){
+//     localStorage.setItem("visitCount",1);
+// }else{
+//     const visitCount= parseInt(localStorage.getItem("visitCount"));
+//     localStorage.setItem("visitCount",visitCount+1)
+// }
+// const visitCount=localStorage.getItem("visitCount");
+
+// const message=`You have visited this site ${ visitCount } times.`;
+// document.getElementById("visitCountMessage").textContent=message;
+// var birthYear=2005
+// var b=`My birth year is ${birthYear} \n Data type of my declared variable is numbre `
+// document.write(b)
+// var VisitorsName="<b>John Doe<b>"
+// var productTitle="XYZ Clothing Store"
+// var order=" ordered <b>5T-shirt<b>(s) on "
+// document.write(VisitorsName+order+productTitle)
+
+
+// CHAPTER-14 to 16
+// let studentsName=[]
+// let studentData={
+//     name:[],
+// }
+
+// let strings=["apple","banana ","orange"]
+// let numbre=[ 33,44,66,99,67,88,54,]
+// let booleanarray=[true,false,true,false]
+// let mixedarray=["atif",23,true,"strue",fale{name:"atif",age:45}]
+// let Qualification=["SSC","HSC","BCS","BS","BCOM","MS","M.PHIL.","PHD"]
+// let listElement=document.getElementById('list')
+// Qualification.forEach(item=>
+//     {
+//      const listItem =document.createElement("li")
+//      listItem.textContent=item;
+//      listElement.appendChild(listItem);
+//     })
+
+
+
+// let studentNames =["Atif","Rafay","Salman"];
+// let studentScore=[320,230,480];
+// for(let i=0;i<studentNames.length;i++){
+//     let name=studentNames[i]
+//     let score=studentScore[i];
+//     let percentage=(score/500)*100;
+    
+//     console.log(`Score of ${name} is ${score}.Percentage:${percentage}%`)
+// }
+
+
+// let studentScore2=[320,230,550,660,234]
+// console.log(`Students score is ${studentScore2}`)
+// studentScore2.sort();
+// console.log(`Ordered scores of Students:${studentScore2}`)
+
+
+
+// let cityNames=["Karachi","Lahore","Peshawar","Quetta","Islamabad"]
+// let selectedCities=[cityNames[0],cityNames[2],cityNames[4]]
+// console.log(`Cities list:\n${cityNames}`)
+// console.log(`Selected cities list:\n${selectedCities}`)
+
+
+// let arr=[" These "," are "," my "," shoes "]
+// let b=arr[0]+arr[1]+arr[2]+arr[3]
+// console.log(`String: ${b}`)
+
+
+// let Phonemanufactures=["Apple","Samsung","Motorola","Nokia","Sony","Haier"]
+// document.write("<select>")
+// for(let i=0;i  < Phonemanufactures.length;i++){
+//     document.write(`<Phonemanufacture value="${i}">${Phonemanufactures[i]}</Phonemanufacture>`);
+// }
+// document.write("</select>");
+
+
+// CHAPTER-5
+
+
+
+
+
+
+
+
+
+
+
